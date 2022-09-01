63bcaf8d710186af07950fe395b171e95e96791a
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-09-01 14:35:25 +0800

    use contains = sql like %% or indexOf

--- src/main/java/com/example/aws_db/controller/TestController.java
+++ src/main/java/com/example/aws_db/controller/TestController.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.example.aws_db.domain.AWSDb;
 import com.example.aws_db.repo.AwsDBRepo;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,10 @@ public class TestController {
     public String testInsert() {
         AWSDb awsDb = new AWSDb();
         awsDb.setId("12333");
-        awsDb.setJsonData("testAutoDetectId");
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("prop1", "testAutoDetectId");
+        Gson gson = new Gson();
+        awsDb.setJsonData(gson.toJson(jsonObject));
         awsDBRepo.save(awsDb);
 
         return awsDb.getId();
@@ -31,7 +36,10 @@ public class TestController {
     @RequestMapping("/test")
     public AWSDb test() {
         AWSDb awsDb = awsDBRepo.getById("f6202d5d-6976-440b-be64-438dfe962df3");
-        awsDb.setJsonData("updateByHere");
+        JsonObject jsonObject = new JsonObject();
+        Gson gson = new Gson();
+        awsDb.setJsonData(gson.toJson(jsonObject));
+        jsonObject.addProperty("prop1", "updateByHere");
         awsDBRepo.update("f6202d5d-6976-440b-be64-438dfe962df3", awsDb);
 
         return awsDb;
@@ -46,15 +54,16 @@ public class TestController {
     @RequestMapping("/query")
     public PaginatedScanList<AWSDb> query() {
         String id = "1233333333333333333333333";
-        String jsonData = "updated";
-        int n = 12333;
+        String jsonData = "prop1";
+        int n = 123333;
 
         Map<String, AttributeValue> eav = new HashMap<>();
         eav.put(":v1", new AttributeValue(id));
         eav.put(":v2", new AttributeValue(jsonData));
         eav.put(":v3", new AttributeValue(String.valueOf(n)));
 
-        String query = "side_project_db = :v1 or jsonData = :v2 or side_project_db = :v3";
+        String query = "side_project_db = :v1 or contains(jsonData, :v2) or side_project_db = :v3";
+
         return awsDBRepo.query(eav, query);
     }
 }

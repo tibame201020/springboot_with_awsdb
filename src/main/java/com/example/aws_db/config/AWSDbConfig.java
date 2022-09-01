5cc244cbb7d37404721dc31eabe29086aec37d0c
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-09-01 13:34:45 +0800

    簡化AmazonDynamoDB config 提供Region pubkey prikey即可

--- src/main/java/com/example/aws_db/config/AWSDbConfig.java
+++ src/main/java/com/example/aws_db/config/AWSDbConfig.java
@@ -2,50 +2,35 @@ package com.example.aws_db.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.s3.model.Region;
+import com.amazonaws.regions.Regions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSDbConfig {
 
-    private static final Region AWS_REGION = Region.AP_Mumbai;
-    // ap-south-1
-    private static final String REGION = AWS_REGION.toAWSRegion().getName();
-    private static final String END_POINT_PREFIX = "dynamodb";
-    // dynamodb.ap-south-1.amazonaws.com
-    private static final String SERVICE_END_POINT = AWS_REGION.toAWSRegion().getServiceEndpoint(END_POINT_PREFIX);
-
     private static final String PUB_KEY = "ur public access key";
     private static final String PRI_KEY = "ur private access key";
 
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(buildAWSDB());
+        return new DynamoDBMapper(buildAmazonDynamoDB());
     }
 
-    private AmazonDynamoDB buildAWSDB() {
+    private AmazonDynamoDB buildAmazonDynamoDB() {
         return AmazonDynamoDBClientBuilder
                 .standard()
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                SERVICE_END_POINT,
-                                REGION
-                        )
-                )
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicAWSCredentials(
-                                        PUB_KEY,
-                                        PRI_KEY
-                                )
-                        )
-                )
+                .withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(createCredentials())
                 .build();
+    }
+
+    private AWSStaticCredentialsProvider createCredentials() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(PUB_KEY, PRI_KEY);
+        return new AWSStaticCredentialsProvider(basicAWSCredentials);
     }
 
 }
